0eb852b16d32fd5f64da7ff675966f971639e4f2
kapaka-m <kapaka-m@users.noreply.github.com>
2026-07-21 01:24:46 +0500

    fix

--- apps/api/tests/TestCase.php
+++ apps/api/tests/TestCase.php
@@ -4,6 +4,18 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+/**
+ * @method \Illuminate\Testing\TestResponse get(string $uri, array $headers = [])
+ * @method \Illuminate\Testing\TestResponse post(string $uri, array $data = [], array $headers = [])
+ * @method \Illuminate\Testing\TestResponse put(string $uri, array $data = [], array $headers = [])
+ * @method \Illuminate\Testing\TestResponse patch(string $uri, array $data = [], array $headers = [])
+ * @method \Illuminate\Testing\TestResponse delete(string $uri, array $data = [], array $headers = [])
+ * @method \Illuminate\Testing\TestResponse getJson(string $uri, array $headers = [], int $options = 0)
+ * @method \Illuminate\Testing\TestResponse postJson(string $uri, array $data = [], array $headers = [], int $options = 0)
+ * @method \Illuminate\Testing\TestResponse putJson(string $uri, array $data = [], array $headers = [], int $options = 0)
+ * @method \Illuminate\Testing\TestResponse patchJson(string $uri, array $data = [], array $headers = [], int $options = 0)
+ * @method \Illuminate\Testing\TestResponse deleteJson(string $uri, array $data = [], array $headers = [], int $options = 0)
+ */
 abstract class TestCase extends BaseTestCase
 {
     //
